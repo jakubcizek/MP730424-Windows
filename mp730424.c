55ddeeaf38fb61817febd63b836a3b10888ae987
Jakub Cizek <cizekuba@gmail.com>
2023-01-26 13:05:35 +0100

    WIn32 timers for requesting loop

--- mp730424.c
+++ mp730424.c
@@ -3,27 +3,74 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <signal.h>
+#include <winuser.h>
+#pragma comment(lib, "user32.lib")
 
-HANDLE port;        // Serial COM port
-BOOL loop = TRUE;   // Serial reader loop
-FILE *fo;           // CSV file writer
+HANDLE port;                            // Serial COM port
+BOOL loop = TRUE;                       // Serial reader loop
+FILE *fo;                               // CSV file writer
+BOOL fileLogging = FALSE;               // Logging to CSV file disabled by default
+HWND requestTimer;                      // Multimeter requesting timer
+UINT_PTR timerId = 0;                   // Multimeter requesting timer ID
+char fileName[50] = "measurements.csv"; // Default logging filename
+char portName[20] = "COM1";             // Default COM port
+char portPath[100] = "\\.\\COM1";       // Default COM port path
+DWORD portSpeed = 115200;               // Default COM baudrate
+DWORD timerDelayMs = 500;               // Default multimeter querying delay
 
-void writeAndReadUntilEOL(HANDLE *port, char *request, DWORD request_size, char *response);    // Write to Serial and wait to response until \n
-void onSignal(int signal);                                                                      // Signal handler
-int main(int argc, char *argv[]);                                                               // App entry
+void requestData(HANDLE *port, char *request, DWORD request_size, char *response); // Write to Serial and wait to response until \n
+void onSignal(int signal);                                                         // SIGINT handle for clean-up and exit(0)
+void CALLBACK requestLoop(HWND hwnd, UINT msg, UINT timer, DWORD time);            // Signal handler
+int main(int argc, char *argv[]);                                                  // App entry
 
 // Signal handler
 void onSignal(int signal)
 {
     if (signal == SIGINT)
     {
-        loop = FALSE;
+        printf("\r\n");
+        printf("Closing timer... ");
+        if (KillTimer(requestTimer, timerId))
+        {
+            printf("OK\r\n");
+        }
+        else
+        {
+            printf("ERROR\r\n");
+        }
+        if (fileLogging)
+        {
+            printf("Closing %s... ", fileName);
+            if (fclose(fo) == 0)
+            {
+                printf("OK\r\n");
+            }
+            else
+            {
+                printf("ERROR\r\n");
+            }
+        }
+        if (port != NULL)
+        {
+            printf("Closing %s.... ", portName);
+            if (CloseHandle(port))
+            {
+                printf("OK\r\n");
+            }
+            else
+            {
+                printf("ERROR\r\n");
+            }
+        }
+
+        printf("Bye bye!\r\n");
+        exit(0);
     }
 }
 
 // Write query to Serial COM and wait for response until \n, or timeout
 // Reading from serial buffer char by char
-void writeAndReadUntilEOL(HANDLE *port, char *request, DWORD request_size, char *response)
+void requestData(HANDLE *port, char *request, DWORD request_size, char *response)
 {
     DWORD sent = 0;
     WriteFile(*port, request, request_size, &sent, NULL);
@@ -48,47 +95,70 @@ void writeAndReadUntilEOL(HANDLE *port, char *request, DWORD request_size, char 
                     break;
                 }
             }
-            else    // If we did not received just 1 char, let's break while loop
+            else // If we did not received just 1 char, let's break while loop
                 break;
         }
     }
 }
 
+void CALLBACK requestLoop(HWND hwnd, UINT msg, UINT timer, DWORD time)
+{
+    // Get current time in hh:mm:ss.ms
+    SYSTEMTIME t;
+    GetLocalTime(&t);
+    char dt[13];
+    sprintf(dt, "%02d:%02d:%02d.%03d", t.wHour, t.wMinute, t.wSecond, t.wMilliseconds);
+
+    // Ask multimeter for its current function via "FUNC?\n" query
+    char response[50];
+    requestData(&port, "FUNC?\n", 6, response);
+    printf("%s\t %s\t", dt, response);
+    if (fileLogging)
+        fprintf(fo, "%s;%s;", dt, response);
+
+    // Ask multimeter for its current measurement via "MEAS?\n" query
+    requestData(&port, "MEAS?\n", 6, response);
+    double number = atof(response);
+    printf("%f\r\n", number);
+    if (fileLogging)
+        fprintf(fo, "%f\n", number);
+}
+
 // App entry
 int main(int argc, char *argv[])
 {
-    char portName[20] = "COM1";             // Default COM port
-    DWORD portSpeed = 115200;               // Default COM baudrate
-    DWORD delayMs = 500;                    // Default multimeter querying delay
-    char fileName[50] = "measurements.csv"; // Default filename
-    BOOL writing = FALSE;                   // Logging to CSV file disabled by default
+    MSG msg;
 
-    signal(SIGINT, onSignal);               // SIGINT registration
+    signal(SIGINT, onSignal); // SIGINT registration
 
     /**
      * Stupid argv parser
      * -p port, COM1, COM2, ...
-     * -s baudrate, 9600, 115200, ...
-     * -d multimeter querying delay in ms
+     * -b baudrate, 9600, 115200, ...
+     * -s multimeter querying speed in ms
      * -f CSV filename for logging
      */
     for (int i = 1; i < argc; i++)
     {
         if (strcmp(argv[i], "-p") == 0)
-            sprintf(portName, "\\\\.\\%s", argv[i + 1]);
-        if (strcmp(argv[i], "-s") == 0)
+        {
+            sprintf(portPath, "\\\\.\\%s", argv[i + 1]);
+            strcpy(portName, argv[i + 1]);
+        }
+        if (strcmp(argv[i], "-b") == 0)
             portSpeed = atoll(argv[i + 1]);
-        if (strcmp(argv[i], "-d") == 0)
-            delayMs = atoll(argv[i + 1]);
+        if (strcmp(argv[i], "-s") == 0)
+            timerDelayMs = atoll(argv[i + 1]);
         if (strcmp(argv[i], "-f") == 0)
         {
             strcpy(fileName, argv[i + 1]);
-            writing = TRUE;
+            fileLogging = TRUE;
         }
     }
 
     // Openning COM port
-    port = CreateFileA(portName,                     // port name
+    printf("Openning port %s... ", portName);
+    port = CreateFileA(portPath,                     // port path
                        GENERIC_READ | GENERIC_WRITE, // Read/Write
                        0,                            // No Sharing
                        NULL,                         // No Security
@@ -97,10 +167,19 @@ int main(int argc, char *argv[])
                        NULL);                        // Null for Comm Devices
 
     if (port == INVALID_HANDLE_VALUE)
-        printf("Can not open %s\r\n", portName);
+        printf("ERROR\r\n");
     else
     {
-        FlushFileBuffers(port); // Flushing port buffers
+        printf("OK\r\n");
+        printf("Flushing port buffers... ", portName);
+        if (FlushFileBuffers(port))
+        {
+            printf("OK\r\n");
+        }
+        else
+        {
+            printf("ERROR\r\n");
+        }
 
         // Port timeouts in ms
         COMMTIMEOUTS timeouts = {0};
@@ -109,7 +188,15 @@ int main(int argc, char *argv[])
         timeouts.ReadTotalTimeoutMultiplier = 0;
         timeouts.WriteTotalTimeoutConstant = 100;
         timeouts.WriteTotalTimeoutMultiplier = 0;
-        SetCommTimeouts(port, &timeouts);
+        printf("Configuring port timers... ");
+        if (SetCommTimeouts(port, &timeouts))
+        {
+            printf("OK\r\n");
+        }
+        else
+        {
+            printf("ERROR\r\n");
+        }
 
         // Serial setup
         DCB state = {0};
@@ -118,50 +205,56 @@ int main(int argc, char *argv[])
         state.ByteSize = 8;
         state.Parity = NOPARITY;
         state.StopBits = ONESTOPBIT;
-        SetCommState(port, &state);
+        printf("Configuring serial connection %d-8-N-1... ", portSpeed);
+        if (SetCommState(port, &state))
+        {
+            printf("OK\r\n");
+        }
+        else
+        {
+            printf("ERROR\r\n");
+        }
 
+        // Requesting multimeter for its ID via "*IDN?\n" query
         char response[100]; // Response buffer
-        
-        // Ask multimeter for its ID via "*IDN?\n" query
-        writeAndReadUntilEOL(&port, "*IDN?\n", 6, response);
-        printf("%s\r\n", response);
+        printf("Requesting name of connected device: ");
+        requestData(&port, "*IDN?\n", 6, response);
+        if (strlen(response) > 0)
+        {
+            printf("%s\r\n", response);
+        }
+        else
+        {
+            printf("Unknown device\r\n");
+        }
 
         // Open file for CSV logging
-        if (writing)
+        if (fileLogging)
         {
+            printf("Openning %s for logging... ", fileName);
             fo = fopen(fileName, "w");
+            if (fo != NULL)
+            {
+                printf("OK\r\n");
+            }
+            else
+            {
+                printf("ERROR\r\n");
+                fileLogging = FALSE;
+            }
         }
 
-        // Querying loop
-        while (loop)
+        printf("\r\n");
+
+        timerId = SetTimer(requestTimer, 1, timerDelayMs, (TIMERPROC)&requestLoop);
+
+        // Messaging loop
+        while (GetMessage(&msg, NULL, 0, 0))
         {
-            // Get current time in hh:mm:ss.ms
-            SYSTEMTIME t;
-            GetLocalTime(&t);
-            char dt[13];
-            sprintf(dt, "%02d:%02d:%02d.%03d", t.wHour, t.wMinute, t.wSecond, t.wMilliseconds);
-
-            // Ask multimeter for its current function via "FUNC?\n" query
-            writeAndReadUntilEOL(&port, "FUNC?\n", 6, response);
-            printf("%s\t %s\t", dt, response);
-            if (writing)
-                fprintf(fo, "%s;%s;", dt, response);
-
-            // Ask multimeter for its current measurement via "MEAS?\n" query
-            writeAndReadUntilEOL(&port, "MEAS?\n", 6, response);
-            double number = atof(response);
-            printf("%f\r\n", number);
-            if (writing)
-                fprintf(fo, "%f\n", number);
-            
-            Sleep(delayMs); // Next query delay
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
         }
-
-        if (writing)
-            fclose(fo);
     }
 
-    CloseHandle(port);  // Close COM port
-
-    return 0; // Bye bye!
+    return 0;
 }
